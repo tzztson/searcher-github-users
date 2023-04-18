8c84f83146fe72967134a340b3e0344be6ca9b01
tzztson <zhenghu61919@gmail.com>
2023-04-18 20:11:19 +0800

    chore: install react-router-dom and react-icons, implement pages and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/layout/Navbar'
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-accent">Warning</button>
-      <button className="btn btn-error">Error</button>
-    </div>
-  );
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <div>I am Home</div>
+    )
+}
+
+export default Home;
