91a6ba03eba970878fccf525909dbd9ee23d6929
tzztson <zhenghu61919@gmail.com>
2023-04-18 17:50:41 +0800

    env: add daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <button className="btn btn-info">Info</button>
+      <button className="btn btn-success">Success</button>
+      <button className="btn btn-warning">Warning</button>
+      <button className="btn btn-error">Error</button>
+    </div>
   );
 }
 
